a92e2cff70d7fb06de4d9ea63c7526f89a904bc2
ihor.shchuka <ihor.shchuka@gmail.com>
2023-08-22 18:35:31 +0300

    Added join

--- services/orders.py
+++ services/orders.py
@@ -1,34 +1,61 @@
 from datetime import datetime
 
-import flask
 from flask import session, render_template, request
 
 from logger import log
+from utils import database
+from utils.db_models import Orders, OrderedDishes, Dishes
 from utils.sql_lite import SQLiteDB
 
 dishes_in_cart = []
 
 
 def get_cart():
+    database.init_db()
     if session.get("user_id"):
-        with SQLiteDB("dish.db") as db:
-            cart = db.select_from("Orders", ["id"],
-                                  where=f"user={session.get('user_id')} AND status=0", fetch_all=False)
+        with (SQLiteDB("dish.db") as db):
+            # cart = db.select_from("Orders", ["id"],
+            #                       where=f"user={session.get('user_id')} AND status=0", fetch_all=False)
+            cart = database.db_session.query(Orders).where(
+                (Orders.user == session["user_id"]) & (Orders.status == 0)).one_or_none()
             if cart:
-                session["cart_id"] = cart["id"]
+                session["cart_id"] = cart.id
                 field_to_select = ("dish_name, category, ccal, fat, carb, description, count, price, photo, "
                                    "Ordered_dishes.id as id")
-                dishes = db.select_from(row_query=f"SELECT {field_to_select} from Ordered_dishes JOIN Dishes on "
-                                                  f"Dishes.id = Ordered_dishes.dish_id WHERE "
-                                                  f"Ordered_dishes.order_id = {cart['id']}")
-                session["dishes_in_cart"] = dishes
+                # dishes = db.select_from(row_query=f"SELECT {field_to_select} from Ordered_dishes JOIN Dishes on "
+                #                                   f"Dishes.id = Ordered_dishes.dish_id WHERE "
+                #                                   f"Ordered_dishes.order_id = {cart['id']}")
+
+                dishes = database.db_session.query(OrderedDishes.id,
+                                                   Dishes.dish_name,
+                                                   Dishes.category,
+                                                   Dishes.ccal,
+                                                   Dishes.fat,
+                                                   Dishes.carb,
+                                                   Dishes.description,
+                                                   OrderedDishes.count,
+                                                   Dishes.price,
+                                                   Dishes.photo,
+                                                   ).join(Dishes, Dishes.id == OrderedDishes.dish_id).filter(
+                    OrderedDishes.order_id == cart.id).all()
+
+                fields = ["id", "dish_name", "category", "ccal", "fat", "carb", "description", "count", "price",
+                          "photo"]
+                ordered_dishes = []
+
                 for dish in dishes:
+                    result_dict = dict.fromkeys(fields)
+                    for key, value in zip(result_dict.keys(), dish):
+                        result_dict[key] = value
+                    ordered_dishes.append(result_dict)
+                session["dishes_in_cart"] = ordered_dishes
+                for dish in ordered_dishes:
                     dish["total"] = dish["count"] * dish["price"]
                     dish["fat"] = dish["count"] * dish["fat"]
                     dish["carb"] = dish["count"] * dish["carb"]
                     dish["ccal"] = dish["count"] * dish["ccal"]
 
-                return render_template("cart.html", dishes=dishes)
+                return render_template("cart.html", dishes=ordered_dishes)
             else:
                 return "Cart is empty"
 
